5cf9ed4446450c65b47a88e60dcaf07150cadc9e
tulio <tulio@tulio>
2023-04-11 21:59:09 -0300

    init

---
+++ plugins/user.lua
@@ -0,0 +1,11 @@
+return {
+ -- You can also add new plugins here as well:
+ -- Add plugins, the lazy syntax
+ -- {
+ --  "catppuccin/nvim",
+ --  as = "catppuccin",
+ --  config = function()
+ --   require("catppuccin").setup {}
+ --  end,
+ -- },
+}
